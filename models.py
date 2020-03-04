a5c4705111feb14a8739595c50be4cac0eca3157
mikel.brostrom <mikel.brostrom@cevt.se>
2020-03-04 16:13:28 +0100

    LSTM model in place

--- models.py
+++ models.py
@@ -28,3 +28,37 @@ class Net(nn.Module):
         x = self.fc2(x)
         output = F.log_softmax(x, dim=1)
         return output
+
+
+# Here we define our model as a class
+class MV_LSTM(nn.Module):
+    def __init__(self, input_dim, hidden_dim, batch_size, output_dim=1,
+                 num_layers=2):
+        super(MV_LSTM, self).__init__()
+        self.input_dim = input_dim
+        self.hidden_dim = hidden_dim
+        self.batch_size = batch_size
+        self.num_layers = num_layers
+
+        # Define the LSTM layer
+        self.lstm = nn.LSTM(self.input_dim, self.hidden_dim, self.num_layers)
+
+        # Define the output layer
+        self.linear = nn.Linear(self.hidden_dim, output_dim)
+
+    def init_hidden(self):
+        # This is what we'll initialise our hidden state as
+        return (torch.zeros(self.num_layers, self.batch_size, self.hidden_dim),
+                torch.zeros(self.num_layers, self.batch_size, self.hidden_dim))
+
+    def forward(self, input):
+        # Forward pass through LSTM layer
+        # shape of lstm_out: [input_size, batch_size, hidden_dim]
+        # shape of self.hidden: (a, b), where a and b both 
+        # have shape (num_layers, batch_size, hidden_dim).
+        lstm_out, self.hidden = self.lstm(input.view(len(input), self.batch_size, -1))
+
+        # Only take the output from the final timetep
+        # Can pass on the entirety of lstm_out to the next layer if it is a seq2seq prediction
+        y_pred = self.linear(lstm_out[-1].view(self.batch_size, -1))
+        return y_pred.view(-1)
--- train.py
+++ train.py
@@ -1,13 +1,50 @@
 from data_loader import LoadData, load_preprocessed_data
-from models import Net
+from models import Net, MV_LSTM
 import torch
-import matplotlib.pyplot as plt
 from tqdm import tqdm
 import torch.optim as optim
 from torch.optim.lr_scheduler import StepLR
 import torch.nn.functional as F
 from torch.utils.tensorboard import SummaryWriter
 import argparse
+from keras.layers import ConvLSTM2D
+
+
+def train2(args, mv_net, device, train_loader, optimizer, epoch, criterion):
+    tb_writer = SummaryWriter()
+    mv_net.train()
+    nb = train_loader.__len__()
+    pbar = tqdm(enumerate(train_loader), total=nb)
+    for batch_idx, (timeseries_data, remaining, current) in pbar:  # batch --------
+        #print(timeseries_data.shape)
+        #print(timeseries_data)
+        #print(remaining.shape)
+        data, remaining_t, _ = timeseries_data.to(device, dtype=torch.float), remaining.to(device, dtype=torch.float), current.to(device, dtype=torch.float)
+        mv_net.init_hidden()
+        output = mv_net(data)
+
+        loss = criterion(output, remaining_t)
+        # backward pass
+        loss.backward()
+        # update parameters
+        optimizer.step()
+        # Zero out gradient,
+        # else they will accumulate between batches
+        optimizer.zero_grad()
+
+        if batch_idx % args.log_interval == 0:
+            print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
+                epoch, batch_idx * len(data), len(train_loader.dataset),
+                100. * batch_idx / len(train_loader), loss.item()))
+
+        # end batch ----------------------------------------------
+
+    # Write Tensorboard results
+    if tb_writer:
+        x = [loss]
+        titles = ['MSE']
+        for xi, title in zip(x, titles):
+            tb_writer.add_scalar(title, xi, epoch)
 
 
 def train(args, model, device, train_loader, optimizer, epoch):
@@ -20,22 +57,21 @@ def train(args, model, device, train_loader, optimizer, epoch):
         #print(timeseries_data)
         #print(remaining.shape)
         data, remaining_t, _ = timeseries_data.to(device, dtype=torch.float), remaining.to(device, dtype=torch.float), current.to(device, dtype=torch.float)
-        optimizer.zero_grad()
+
         output = model(data)
+
         loss = F.mse_loss(output, remaining_t)
         loss.backward()
         optimizer.step()
+        optimizer.zero_grad()
+
         if batch_idx % args.log_interval == 0:
             print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
                 epoch, batch_idx * len(data), len(train_loader.dataset),
                 100. * batch_idx / len(train_loader), loss.item()))
-        #print('cols', len(timeseries_data[0]))
-        #print(timeseries_data.shape)
-        #rint(remaining.shape)
-        #rint(current.shape)
-        #rint(remaining)
-        #rint(current)
-        #print()
+
+        # end batch ----------------------------------------------
+
     # Write Tensorboard results
     if tb_writer:
         x = [loss]
@@ -67,10 +103,20 @@ def main():
     use_cuda = not args.no_cuda and torch.cuda.is_available()
     device = torch.device("cuda" if use_cuda else "cpu")
 
+    n_features = 4  # this is number of parallel inputs
+    n_timesteps = 1000  # this is number of timesteps
+
     model = Net().to(device)
     optimizer = optim.Adam(model.parameters(), lr=args.lr)
-
     scheduler = StepLR(optimizer, step_size=1, gamma=args.gamma)
+
+    lstm_input_size = 1
+
+    LSTM_model = MV_LSTM(8000, hidden_dim=4, batch_size=1, output_dim=1, num_layers=2)
+    LSTM_model.to(device)
+    criterion = torch.nn.MSELoss()  # reduction='sum' created huge loss value
+    optimizer = torch.optim.Adam(LSTM_model.parameters(), lr=0.001)
+
     # data = load_preprocessed_data()
     dataset = LoadData('./data/preprocessed_data.pkl', window=20)
 
@@ -82,7 +128,9 @@ def main():
 
     for epoch in range(1, args.epochs + 1):
         print('\tEPOCH', epoch, '/', args.epochs)
-        train(args, model, device, train_loader, optimizer, epoch)
+        # train(args, model, device, train_loader, optimizer, epoch)
+        train2(args, LSTM_model, device, train_loader, optimizer, epoch, criterion)
+
         # test(args, model, device, test_loader)
         scheduler.step()
 
